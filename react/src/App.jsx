7c5c6e8ff0232462e83742cba9a2375f66cd1e90
David Suh <ssuh10@my.bcit.ca>
2024-08-19 12:36:30 -0700

    Add close button to the season result modal

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -229,6 +229,14 @@ function App() {
               <div className="p-4 md:p-5">
                 {renderSeasonDetails()}
               </div>
+              <div className="flex justify-center p-2">
+                <button
+                  onClick={closeSeasonModal} // Attach the close modal event handler
+                  type="button"
+                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                  Close
+                </button>
+              </div>
             </div>
           </div>
         </div>
